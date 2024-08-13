3451c66afc0dfa7c953cef6ab97fc12d923af8f6
elige <2015805168@qq.com>
2024-08-13 23:37:42 +0800

    init code

---
+++ config/site.ts
@@ -0,0 +1,73 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Next.js + NextUI",
+  description: "Make beautiful websites regardless of your design experience.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "氏族展示",
+      href: "/guilds",
+    },
+    {
+      label: "氏族合照",
+      href: "/group",
+    },
+    {
+      label: "氏族历史",
+      href: "/list",
+    },
+    {
+      label: "网站链接",
+      href: "/link",
+    },
+    {
+      label: "氏族活动",
+      href: "/",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+    {
+      label: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Team",
+      href: "/team",
+    },
+    {
+      label: "Calendar",
+      href: "/calendar",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "Help & Feedback",
+      href: "/help-feedback",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "https://github.com/nextui-org/nextui",
+    twitter: "https://twitter.com/getnextui",
+    docs: "https://nextui.org",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
+  },
+};
